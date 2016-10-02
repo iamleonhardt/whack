bd8ec9377f6f0f5537dcbdd6b423f6e391e0ba12
Bill Leonhardt <iamleonhardt@gmail.com>
2016-10-01 19:46:34 -0700

    added highscore storage

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var bgColor = '#736357';
 var defaultImage = 'mole.png';
 var whackedImage = 'whack.png';
 var highScore = 0;
+var highScoreName = '';
 var score = 0;
 var min = 800;
 var max = 5000;
@@ -9,69 +10,82 @@ var hit = new Audio();
 var miss = new Audio();
 
 
-function playHitSound(){
+function playHitSound() {
     randomHitNum = Math.floor(Math.random() * 3 + 1);
     hit.src = "sounds/hit0" + randomHitNum + ".wav";
     hit.play();
 }
 
-function playMissSound(){
+function playMissSound() {
     randomSwishNum = Math.floor(Math.random() * 4 + 1);
-    miss.src = "sounds/swish"+ randomSwishNum + ".wav";
+    miss.src = "sounds/swish" + randomSwishNum + ".wav";
     miss.play();
 }
 
-$('document').ready(function(){
-
+$('document').ready(function () {
 
     // $('.gameContainer1').append('<div class="startButton">Start Game</div>').click(function(){
     //     $('.startButton').remove();
     //     start();
     // });
 
-    function gameOver(){
+    function gameOver() {
         $('.mole').stop().animate({'right': '100%'}, 1000);
-        $('.score').html('Score: ' + score);
-        $('.score').append('<div class="tryAgain">Game Over | Try Again</div>');
-        $('.tryAgain').click(function(){
-            start();
-            $('.tryAgain').remove;
-        })
+        // $('.score').html('Score: ' + score);
+        if (score >= highScore) {
+            highScore = score;
+            $('.highScore').html('High Score: ' + highScore);
+            console.log('score is : ', score, 'highScore is : ', highScore);
+            $('.highScoreName').html('<div class="congrats">Congratulations! You have the high Score! <br>Please enter your name: </div><input id="highScoreName"><div class="enterButton">Enter</div>');
+            $('.enterButton').click(function () {
+                highScoreName = $('#highScoreName').val();
+                console.log('highScoreName is : ', highScoreName);
+                $('.highScoreName').html('Player: ' + highScoreName)
+            });
+        }
+
+
+
+    $('.score').append('<div class="tryAgain">Game Over | Try Again</div>');
+    $('.tryAgain').click(function () {
+        start();
+        $('.tryAgain').remove;
+    })
     }
 
-      function start() {
-        score = 0;
-        $('.score').html('Score: ' + score );
+function start() {
+    score = 0;
+    $('.score').html('Score: ' + score);
+    $('.mole').animate({'right': '10%'}, 2500, function () {
+        gameOver();
+    });
+}
 
-        $('.mole').animate({'right': '10%'}, 2500, function () {
+$('.mole').hover(function () {
+    $(this).css({'background-image': 'url(images/' + whackedImage + ')'});
+    score = score + 1;
+    $('.score').html('Score: ' + score);
+    playHitSound();
+    $(this).stop().animate({'right': '102%'}, 1000, function () {
+        $(this).css({'background-image': 'url(images/' + defaultImage + ')'});
+        speed = Math.floor(Math.random() * (max - min) + min);
+        console.log('speed is : ', speed);
+        $(this).animate({'right': '10%'}, speed, function () {
             gameOver();
         });
-    }
-
-    $('.mole').hover(function(){
-        $(this).css({'background-image': 'url(images/'+ whackedImage + ')'});
-        score = score + 1;
-        $('.score').html('Score: ' + score );
-        playHitSound();
-        $(this).stop().animate({'right': '102%'}, 1000, function(){
-            $(this).css({'background-image': 'url(images/'+ defaultImage + ')'});
-            speed = Math.floor(Math.random() * (max - min) + min);
-            console.log('speed is : ', speed);
-            $(this).animate({'right': '10%'}, speed, function () {
-                gameOver();
-            });
-        });
-
     });
-
-    $('.gameContainer1').click(function(){
-        playMissSound();
-        console.log('randomSwishNum is : ', randomSwishNum, 'and miss.src :  ', miss.src);
-
-    });
-
-    $('.mole').css({'background-color': bgColor, 'background-image': 'url(images/'+ defaultImage +')'});
-
-    start();
 
 });
+
+$('.gameContainer1').click(function () {
+    playMissSound();
+    console.log('randomSwishNum is : ', randomSwishNum, 'and miss.src :  ', miss.src);
+
+});
+
+$('.mole').css({'background-color': bgColor, 'background-image': 'url(images/' + defaultImage + ')'});
+
+start();
+
+})
+;
