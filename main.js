b3f81a04d5c31e1c0043f950db46e12c2bc22117
Bill Leonhardt <iamleonhardt@gmail.com>
2016-10-01 21:36:20 -0700

    added random faces

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-var bgColor = '#736357';
-var defaultImage = 'mole.png';
-var whackedImage = 'whack.png';
+var randNum = 1;
+var bgColor = '#d6d6d5';
+var bgColorHit = '#ef4055';
+var defaultImage = 'face' + randNum + '.png';
+var whackedImage = 'hit' + randNum + '.png';
 var highScore = 0;
 var highScoreName = '';
 var score = 0;
-var min = 800;
-var max = 5000;
+var min = 600;
+var max = 4000;
 var hit = new Audio();
 var miss = new Audio();
 
 
+function randomize(){
+    randNum = Math.floor(Math.random() * 3 + 1);
+}
+
 function playHitSound() {
-    randomHitNum = Math.floor(Math.random() * 3 + 1);
-    hit.src = "sounds/hit0" + randomHitNum + ".wav";
+    randomize();
+    hit.src = "sounds/hit0" + randNum + ".wav";
+    console.log('randNum is : ', randNum, 'and hit.src is : ', hit.src);
     hit.play();
 }
 
 function playMissSound() {
-    randomSwishNum = Math.floor(Math.random() * 4 + 1);
-    miss.src = "sounds/swish" + randomSwishNum + ".wav";
+    randomize();
+    miss.src = "sounds/swish" + randNum + ".wav";
     miss.play();
 }
 
@@ -45,47 +52,53 @@ $('document').ready(function () {
         }
 
 
-
-    $('.score').append('<div class="tryAgain">Game Over | Try Again</div>');
-    $('.tryAgain').click(function () {
-        start();
-        $('.tryAgain').remove;
-    })
+        $('.score').append('<div class="tryAgain">Game Over | Try Again</div>');
+        $('.tryAgain').click(function () {
+            start();
+            $('.tryAgain').remove;
+        })
     }
 
-function start() {
-    score = 0;
-    $('.score').html('Score: ' + score);
-    $('.mole').animate({'right': '10%'}, 2500, function () {
-        gameOver();
-    });
-}
 
-$('.mole').hover(function () {
-    $(this).css({'background-image': 'url(images/' + whackedImage + ')'});
-    score = score + 1;
-    $('.score').html('Score: ' + score);
-    playHitSound();
-    $(this).stop().animate({'right': '102%'}, 1000, function () {
-        $(this).css({'background-image': 'url(images/' + defaultImage + ')'});
-        speed = Math.floor(Math.random() * (max - min) + min);
-        console.log('speed is : ', speed);
-        $(this).animate({'right': '10%'}, speed, function () {
+
+    function start() {
+        score = 0;
+        $('.score').html('Score: ' + score);
+        $('.mole').animate({'right': '10%'}, 2500, function () {
             gameOver();
         });
+    }
+
+    $('.mole').hover(function () {
+        $(this).css({'background-image': 'url(images/hit' + randNum + '.png)'});
+        $(this).css({'background-color': bgColorHit});
+
+        score = score + 1;
+        $('.score').html('Score: ' + score);
+        playHitSound();
+        $(this).stop().animate({'right': '102%'}, 1000, function () {
+            randomize();
+            $(this).css({'background-image': 'url(images/face' + randNum + '.png'});
+            $(this).css({'background-color': bgColor});
+
+            speed = Math.floor(Math.random() * (max - min) + min);
+            console.log('speed is : ', speed);
+            $(this).animate({'right': '10%'}, speed, function () {
+                gameOver();
+            });
+        });
+
     });
 
-});
+    $('.gameContainer1').click(function () {
+        playMissSound();
+        console.log('randomSwishNum is : ', randomSwishNum, 'and miss.src :  ', miss.src);
 
-$('.gameContainer1').click(function () {
-    playMissSound();
-    console.log('randomSwishNum is : ', randomSwishNum, 'and miss.src :  ', miss.src);
+    });
+    randomize();
+    $('.mole').css({'background-color': bgColor, 'background-image': 'url(images/' + defaultImage + ')'});
 
-});
-
-$('.mole').css({'background-color': bgColor, 'background-image': 'url(images/' + defaultImage + ')'});
-
-start();
+    start();
 
 })
 ;
